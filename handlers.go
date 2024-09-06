8497770aae82be8986ed75e719ebce2961f7ef28
Kosench <kosenko.id@dvfu.ru>
2024-09-06 16:01:37 +1000

    Add deleteHandler function

--- handlers.go
+++ handlers.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 const PORT = ":1234"
@@ -13,5 +14,31 @@ func DefaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	Body := "Thanks for visiting!\n"
 	fmt.Fprintf(w, "%s", Body)
+}
 
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	// Get telephone
+	paramStr := strings.Split(r.URL.Path, "/")
+	fmt.Println("Path:", paramStr)
+	if len(paramStr) < 3 {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintln(w, "Not found: "+r.URL.Path)
+		return
+	}
+
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+
+	telephone := paramStr[2]
+	err := deleteEntry(telephone)
+	if err != nil {
+		fmt.Println(err)
+		Body := err.Error() + "\n"
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "%s", Body)
+		return
+	}
+
+	Body := telephone + " deleted!\n"
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s", Body)
 }
