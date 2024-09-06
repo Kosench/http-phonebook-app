f37e4bb55815cb9e4cd7ac9f53f2c2d01fb97098
Kosench <kosenko.id@dvfu.ru>
2024-09-06 16:27:35 +1000

    Add core functions and initialize http.ServeMux

--- handlers.go
+++ handlers.go
@@ -9,7 +9,7 @@ import (
 
 const PORT = ":1234"
 
-func DefaultHandler(w http.ResponseWriter, r *http.Request) {
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Serving:", r.URL.Path, "from", r.Host)
 	w.WriteHeader(http.StatusOK)
 	Body := "Thanks for visiting!\n"
