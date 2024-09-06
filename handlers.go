3f40ee53c984f902f7e1bdd2bb3f6630d0ccd7ef
Kosench <kosenko.id@dvfu.ru>
2024-09-06 15:54:31 +1000

    Add defaultHandler function

--- handlers.go
+++ handlers.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const PORT = ":1234"
+
+func DefaultHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusOK)
+	Body := "Thanks for visiting!\n"
+	fmt.Fprintf(w, "%s", Body)
+
+}
